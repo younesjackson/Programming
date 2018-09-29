180341e9015f4822887ee52c0064382e246600ff
younesjackson <younes.abou@student.hu.nl>
2018-09-29 19:56:10 +0200

    initial commit

---
+++ input.py
@@ -0,0 +1,5 @@
+uurloon = float(input('wat verdien je per uur? '))
+uren = float(input('hoeveel uur heb je gewerkt? '))
+totaal = uurloon * uren
+tekst = str(uren) + ' uur werken levert ' + str(totaal) + ' Euro op.'
+print(tekst)
